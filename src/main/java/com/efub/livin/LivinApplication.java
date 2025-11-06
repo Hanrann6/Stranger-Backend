f6b0aa46d65b83bff3814e82349c9b1d7619069e
sohyu-na <solim73@naver.com>
2025-11-06 18:24:47 +0900

    init: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/efub/livin/LivinApplication.java
@@ -0,0 +1,13 @@
+package com.efub.livin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efub/livin/LivinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.livin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
