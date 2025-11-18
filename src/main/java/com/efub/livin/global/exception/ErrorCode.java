56c50cb250067a58b992078222baee2c63232d7a
sohyu-na <solim73@naver.com>
2025-11-18 17:30:19 +0900

    feat: 회원 가입 관련 에러코드 추가

--- src/main/java/com/efub/livin/global/exception/ErrorCode.java
+++ src/main/java/com/efub/livin/global/exception/ErrorCode.java
@@ -8,9 +8,14 @@ import lombok.Getter;
 public enum ErrorCode {
     UNAUTHORIZED_ACCESS(401, "인증되지 않은 사용자입니다."),
 
+    // 회원 가입 관련
+    NICKNAME_DUPLICATED(409, "이미 존재하는 닉네임입니다."),
+    EMAIL_NOT_VERIFIED(400, "해당 이메일로 요청한 사용자 정보가 없습니다."),
+
     // House 저장 관련
     KAKAO_API_ERROR(500, "카카오 API 연동 중 오류가 발생했습니다."),
     KAKAO_API_EMPTY_RESPONSE(500, "카카오 API로부터 빈 응답을 받았습니다.");
+
 
     private final int status;
     private final String message;
