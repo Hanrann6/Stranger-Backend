556965a314c604c471006259db637c028761a542
sohyu-na <solim73@naver.com>
2025-11-18 21:15:10 +0900

    feat: jwt 인증 적용 login 구현

--- src/main/java/com/efub/livin/global/exception/ErrorCode.java
+++ src/main/java/com/efub/livin/global/exception/ErrorCode.java
@@ -12,6 +12,10 @@ public enum ErrorCode {
     NICKNAME_DUPLICATED(409, "이미 존재하는 닉네임입니다."),
     EMAIL_NOT_VERIFIED(400, "해당 이메일로 요청한 사용자 정보가 없습니다."),
 
+    // 인증 관련
+    USER_NOT_FOUND(404, "존재하지 않는 사용자입니다."),
+    INVALID_TOKEN(401, "유효하지 않는 토큰입니다."),
+
     // House 저장 관련
     KAKAO_API_ERROR(500, "카카오 API 연동 중 오류가 발생했습니다."),
     KAKAO_API_EMPTY_RESPONSE(500, "카카오 API로부터 빈 응답을 받았습니다.");
